18b4dd0154f438613ebe43f99dba5787b9f6df78
dut0va <148580644+dut0va@users.noreply.github.com>
2023-12-29 23:31:41 +0700

    Create good.py

---
+++ good.py
@@ -0,0 +1,12 @@
+from typing import Union, Annotated
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+class CreateUser(BaseModel):
+    username: str
+    email: str
